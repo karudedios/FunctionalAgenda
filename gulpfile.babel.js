c86efb63e21da4357891a94f0e51caf1f72ed0ce
Carlos de Dios <c.dedios@outlook.com>
2015-11-02 22:41:40 -0400

    Configuring browser-watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,16 +5,31 @@ import sass from 'gulp-sass';
 import jade from 'gulp-jade';
 import copy from 'gulp-copy';
 import sourcemaps from 'gulp-sourcemaps';
+import browserSyncModule from 'browser-sync';
 
-let paths = {
-  features: ['src/feature/**/*.js'],
-  templates: ['src/**/*.jade'],
-  scripts: ['static/script/*'],
-  styles: ['src/style/**/*'],
-  images: ['static/img/*'],
-  fonts: ['static/font/*'],
-  tests: ['/tests/**/*']
-};
+
+let browserSync = browserSyncModule.create();
+
+gulp.task('serve', ['compile:scss', 'compile:jade'], () => {
+  browserSync.init({
+    online: false,
+    open: false,
+    port: 3000,
+    server: {
+      baseDir: ['.'],
+      middleware: (req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+      }
+    }
+  });
+
+  gulp.watch('src/**/*.jade', ['compile:jade']);
+  gulp.watch('src/**/*.scss', ['compile:scss']);
+  gulp.watch('src/**/*.js', ['move:js']);
+
+  gulp.watch(['dist/**/*.html', 'src/**/*.js']).on('change', browserSync.reload);
+});
 
 gulp.task('clean', ['clean:html', 'clean:css', 'clean:js']);
 gulp.task('clean:html', () => del(['compiled/**/*.html']));
@@ -26,7 +41,8 @@ gulp.task('compile:jade', ['clean:html'], () => {
     .pipe(sourcemaps.init())
       .pipe(jade())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('compiled/'));
+    .pipe(gulp.dest('compiled/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('compile:scss', ['clean:css'], () => {
@@ -34,16 +50,11 @@ gulp.task('compile:scss', ['clean:css'], () => {
     .pipe(sourcemaps.init())
       .pipe(sass())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('compiled/'));
+    .pipe(gulp.dest('compiled/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('move:js', ['clean:js'], () => gulp.src(['src/**/*.js']).pipe(copy('compiled/', { prefix: 1 })));
 gulp.task('compile', ['compile:scss', 'compile:jade', 'move:js']);
 
-gulp.task('watch', () => {
-  gulp.watch(['src/**/*.jade'], ['compile:jade']);
-  gulp.watch(['src/**/*.scss'], ['compile:scss']);
-  gulp.watch(['src/**/*.js'], ['move:js']);
-});
-
-gulp.task('default', ['watch', 'compile']);
+gulp.task('default', ['serve']);
